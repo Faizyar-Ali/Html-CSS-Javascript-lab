c722b73b74a81e5efbda4e3a7693775dafee7194
Faizyar Ali <shaikhfaizy84@gmail.com>
2025-10-16 22:08:01 +0500

    feat: add day07 basics(variables,if else,objects)

---
+++ day07(java script start)/script.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+var a=2;
+var b=4;
+var c=" hello";
+console.log("This is Mark",a+b+c);
+console.log(typeof a,typeof b,typeof c );
+if(b==3){
+    console.log("True");
+}
+else if(b==4){
+    console.log("Again")
+}
+else{
+
+    console.log("False");
+}
+const f={
+    name:"Faizyar",
+    Age:78,
+    "Capable of":"Learning"
+}
+console.log(f)
+f.Hello="Yellow"
+console.log(f)
+f.name="Faras"
+console.log(f)
